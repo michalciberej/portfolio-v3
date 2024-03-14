a76704cf05940cf137f3cf16a831fc9abadfb8bc
Loko <111128711+michalciberej@users.noreply.github.com>
2024-03-14 09:00:47 +0000

    rest of changes

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,9 +1,9 @@
-import S from './Hero.module.scss';
+import S from "./Hero.module.scss";
 
 const Hero = () => {
   return (
     <div className={S.container}>
-      <h1 className={S.heading}>MICHAL</h1>
+      <h2 className={S.heading}>FrontEnd Developer</h2>
     </div>
   );
 };
